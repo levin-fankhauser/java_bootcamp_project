91496c1d42d020c8a299b4691d9b07916f2090a6
levin-fankhauser <levin.fankhauser@gmx.ch>
2023-06-21 14:38:14 +0200

    login()

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,7 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-
+        login();
     }
 
     public static void createUser() {
@@ -19,22 +19,23 @@ public class Main {
         List<String> allLines = Files.readAllLines(myFile.toPath());
 
         String checkUsername = Terminal.askString("Username: ");
+        String checkPassword = Terminal.askString("Password: ");
 
         //Abfrage des Benutzers
-        String[] lineArray = new String[4];
+        String[] lineArray = new String[5];
 
         for (int i = 0; i < allLines.size(); i++) {
             String line = allLines.get(i);
             lineArray = line.split(";");
-            if (lineArray[0].equals(checkUsername)) {
+            if (lineArray[0].equals(checkUsername) && lineArray[1].equals(checkPassword)) {
                 break;
             }
         }
 
         String username = lineArray[0];
-        double task1 = Double.parseDouble(lineArray[1]);
-        double task2 = Double.parseDouble(lineArray[2]);
-        double task3 = Double.parseDouble(lineArray[3]);
+        double task1 = Double.parseDouble(lineArray[2]);
+        double task2 = Double.parseDouble(lineArray[3]);
+        double task3 = Double.parseDouble(lineArray[4]);
 
         System.out.println("Activity from user " + username + "\nTask1: " + task1 + "h\nTask2: " + task2 + "h\nTask3: " + task3 + "h");
 
