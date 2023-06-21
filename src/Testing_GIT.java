dd92b51245277d33a71fd5daa5397ef5ab236d02
levin-fankhauser <levin.fankhauser@gmx.ch>
2023-06-21 13:57:31 +0200

    Test

--- src/Testing_GIT.java
+++ src/Testing_GIT.java
@@ -1,6 +1,6 @@
 public class Testing_GIT {
     public static void main(String[] args) {
-        System.out.println();
+        System.out.println("Levin");
     }
 
     public static void test() {
