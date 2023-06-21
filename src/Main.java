6848f0cc5bd537f6f63970dfcb8667a8ef0db44a
levin-fankhauser <levin.fankhauser@gmx.ch>
2023-06-21 15:17:43 +0200

    login()

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.StandardOpenOption;
 import java.sql.SQLOutput;
 import java.util.List;
 
@@ -51,16 +52,22 @@ public class Main {
 
         //Abfrage des Benutzers
         String[] lineArray = new String[5];
+        int actualLine = 0;
+        int loginSucessful = 0;
 
         for (int i = 0; i < allLines.size(); i++) {
             String line = allLines.get(i);
             lineArray = line.split(";");
             if (lineArray[0].equals(checkUsername) && lineArray[1].equals(checkPassword)) {
+                actualLine = i;
+                loginSucessful = 1;
                 break;
-            } else {
-                System.out.println("Username or Password not valid!");
-                return;
             }
+        }
+
+        if (loginSucessful == 0) {
+            System.out.println("Username or password not vaild!");
+            return;
         }
 
         String username = lineArray[0];
@@ -70,10 +77,28 @@ public class Main {
 
         System.out.println("Activity from user " + username + "\nTask1: " + task1 + "h\nTask2: " + task2 + "h\nTask3: " + task3 + "h");
 
+        addWorktime(username, task1, task2, task3, actualLine);
     }
 
-    public static void addWorktime() {
+    public static void addWorktime(String username, double task1_old, double task2_old, double task3_old, int actualLine) throws IOException {
+        String myffilePath = "resources/worktime.csv";
+        File myFile = new File(myffilePath);
+        List<String> allLines = Files.readAllLines(myFile.toPath());
 
+        double task1 = Terminal.askDouble("How many hours did you work on task 1: ");
+        task1 = task1_old + task1;
+
+        double task2 = Terminal.askDouble("How many hours did you work on task 2: ");
+        task2 = task2_old + task2;
+
+        double task3 = Terminal.askDouble("How many hours did you work on task 3: ");
+        task3 = task3_old + task3;
+
+        String promt = (username+";"+task1+";"+task2+";"+task3);
+
+        FileWriter writer = new FileWriter(myFile, true);
+        allLines.set(actualLine, promt);
+        Files.write(myFile.toPath(), allLines, StandardOpenOption.WRITE, StandardOpenOption.TRUNCATE_EXISTING);
     }
 
     public static void showDiagram() {
