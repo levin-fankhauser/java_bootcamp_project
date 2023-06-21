7667df93d323ab2d6ed8c2c2fea5f7fca080046a
levin-fankhauser <levin.fankhauser@gmx.ch>
2023-06-21 13:28:23 +0200

    ADD example File

---
+++ src/Files_rwx.java
@@ -0,0 +1,30 @@
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.List;
+
+public class Files_rwx {
+    public static void main(String[] args) throws IOException {
+
+        //Einlesen
+        String myffilePath = "resources/questionsAnswersFile.csv";
+
+        File myFile = new File(myffilePath);
+        System.out.println(myFile.getAbsoluteFile());
+
+        //Ausgeben
+        List<String> allLines = Files.readAllLines(myFile.toPath());
+
+        for (int i = 0; i < allLines.size(); i++) {
+            String line = allLines.get(i);
+            System.out.println(line);
+        }
+
+        //Schreiben
+        FileWriter writer = new FileWriter(myFile, true);
+        writer.write("\n neue Eingabe!?");
+        writer.flush();
+
+    }
+}
