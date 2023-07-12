25f8fadd9e5827e97347a370c5002aa0a966f666
levin-fankhauser <levin.fankhauser@gmx.ch>
2023-07-12 08:40:57 +0200

    Refactor

--- src/MainGUI.java
+++ src/MainGUI.java
@@ -10,8 +10,8 @@ public class MainGUI {
 
         f.add(b);
 
-        f.setSize(400,500);//400 width and 500 height
-        f.setLayout(null);//using no layout managers
-        f.setVisible(true);//making the frame visible  
+        f.setSize(400,500);
+        f.setLayout(null);
+        f.setVisible(true);
     }
 }
